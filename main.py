143a39b6c91b70c409500a107c5e3200cd3353a5
Matt <99276377+mjernst31@users.noreply.github.com>
2025-02-15 22:13:35 +0000

    Doin some things

--- main.py
+++ main.py
@@ -5,19 +5,19 @@ import time
 import spotipy
 import os
 import sys
-#import msvcrt
+import time
+import msvcrt
 
 from spotipy.oauth2 import SpotifyOAuth
 from dotenv import load_dotenv
-#from win11toast import notify
+from win11toast import notify
 
 from stations import stations
-from stations import active_station
 
 load_dotenv()
 
-stationID = active_station     #Set your active station here (Can be modified in stations.py)
-buffer = ""                    #Stores last song that the API called (Prevents repeat songs each API call)
+global_buffer = ""             #Stores last song that the API called (Prevents repeat songs each API call)
+global_stationID = ""
 
 auth_manager = SpotifyOAuth(
     client_id=os.getenv('SPOTIFY_CLIENT_ID'),
@@ -29,43 +29,43 @@ auth_manager = SpotifyOAuth(
 sp = spotipy.Spotify(auth_manager=auth_manager)
 
 def getSongLink():
-    url = "https://xmplaylist.com/api/station/" + stationID
-    
+    global global_stationID
+    url = "https://xmplaylist.com/api/station/" + global_stationID
+
     try:
+        time.sleep(1)
         response = requests.get(url)
         response.raise_for_status()
         data = response.json()
+        print(response)
         
-        #Parses the return string for the Spotify URI.
-        songURI = "spotify:track:" + data['results'][0]['spotify']['id']
-        songTitle = data['results'][0]['track']['title']
-        songArtist = data['results'][0]['track']['artists'][0]
-        songImage = data['results'][0]['spotify']['albumImageLarge']
-
-        songData = {
-            'URI': songURI,
-            'Title': songTitle,
-            'Artist': songArtist,
-            'Image': songImage
+        spotify_uri = {
+            'URI': "spotify:track:" + data['results'][0]['spotify']['id'],
+            'Title': data['results'][0]['track']['title'],
+            'Artist': data['results'][0]['track']['artists'][0],
+            'Image': data['results'][0]['spotify']['albumImageLarge']
         }
-
-        return songData
+        if spotify_uri['URI'] and spotify_uri['URI'] != global_buffer:
+            sp.add_to_queue(spotify_uri['URI'])
+            global_buffer = spotify_uri['URI']
+            icon = {
+                'src': spotify_uri['Image'],
+                'placement': 'appLogoOverride'
+            }
+            print(f"Added {spotify_uri['Title']} by {spotify_uri['Artist']} to queue.")
+        return spotify_uri
 
     except requests.exceptions.RequestException as e:
         print(f"Error fetching data: {e}")
         return None
 
 def main():
-    global buffer
-
-    #Type Prompt for Channel (Refer to stations.py)
+    global global_buffer, global_stationID 
     print("Enter Desired Station: ")
-
-    #Wait for keyboard input for station selection, as a string
     station = input()
 
     if station in stations:
-        stationID = stations[station]
+        global_stationID = stations[station]
     else:
         print("Invalid Station")
         sys.exit()
@@ -73,29 +73,11 @@ def main():
     print("Press 'q' to quit")
     
     while True:
-        #Checks keyboard input
-        #if msvcrt.kbhit():
-        #    key = msvcrt.getch().decode().lower()
-        #    if key == 'q':  #Press 'q' to quit
-        #        sys.exit()
+        if msvcrt.kbhit():
+            key = msvcrt.getch().decode().lower()
+            if key == 'q':  #Press 'q' to quit
+                sys.exit()
     
-        spotify_uri = getSongLink()
-        artist = spotify_uri['Artist']
-        song = spotify_uri['Title']
-        album = spotify_uri['Image']
-        spotify_uri = spotify_uri['URI']
-        
-        #If the song is new, add to queue, and send notif
-        if spotify_uri and spotify_uri != buffer: # 
-            sp.add_to_queue(spotify_uri)
-            buffer = spotify_uri
-
-            icon = {
-                'src': album,
-                'placement': 'appLogoOverride'
-            }
-
-            #notify('xmReader: Up Next!', f'{song} by {artist}', icon=icon)
-            print(f"Added {song} by {artist} to queue.")
+        getSongLink()
 
 main()
