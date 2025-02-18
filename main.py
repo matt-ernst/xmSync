d1eb8d82fe2d59880a2737556dd8c244c5d5afa0
Matt <mjernst31@comcast.net>
2025-02-17 23:06:49 -0500

    Timeout, exception handling, xmSync rebrand

--- main.py
+++ main.py
@@ -7,42 +7,34 @@ import msvcrt
 
 from spotipy.oauth2 import SpotifyOAuth
 from dotenv import load_dotenv
-#from win11toast import notify
+from datetime import datetime
 
 from stations import stations
 
-load_dotenv()
 
 global_buffer = ""             #Stores last song that the API called (Prevents repeat songs each API call)
-global_stationID = ""
-
-auth_manager = SpotifyOAuth(
-    client_id=os.getenv('SPOTIFY_CLIENT_ID'),
-    client_secret=os.getenv('SPOTIFY_CLIENT_SECRET'),
-    redirect_uri=os.getenv('REDIRECT_URI'),
-    scope='user-modify-playback-state'
-)
-
-sp = spotipy.Spotify(auth_manager=auth_manager)
+global_stationID = ""          #Stores the station ID that the user wants to listen to
+global_sp = None               #Spotify API Object
 
 def getSongLink():
-    global global_stationID, global_buffer
-    url = "https://xmplaylist.com/api/station/" + global_stationID
+    global global_stationID, global_buffer, global_sp
 
     try:
+        url = "https://xmplaylist.com/api/station/" + global_stationID
         response = requests.get(url, timeout=5)
         response.raise_for_status()
         data = response.json()
     
-        #Validates that there is a playable song
+        #Validates That There is a Playable Song
         if (data and 'results' in data and data['results'] and 'spotify' in data['results'][0] and 'track' in data['results'][0]):
-        
             spotify_uri = {
                 'URI': "spotify:track:" + data['results'][0]['spotify']['id'],
                 'Title': data['results'][0]['track']['title'],
                 'Artist': data['results'][0]['track']['artists'][0],
                 'Image': data['results'][0]['spotify']['albumImageLarge']
             }
+        
+        #No Playable Song, Returns None, Exception is Triggered.
         else:
             return None
         
@@ -51,36 +43,49 @@ def getSongLink():
         changeStation()
         return None
 
-    #Ensures that the song given by the API is a new song, not the previous
+    #Compares the most recent song xmPlaylist returned with what is currently in queue. If they are different, the song is added to the queue
     if spotify_uri['URI'] and spotify_uri['URI'] != global_buffer:
-        sp.add_to_queue(spotify_uri['URI'])
-        global_buffer = spotify_uri['URI']
-        icon = {
-            'src': spotify_uri['Image'],
-            'placement': 'appLogoOverride'
-        }
+        try:
+            global_sp.add_to_queue(spotify_uri['URI'])
+            global_buffer = spotify_uri['URI']
+            
+            icon = {
+                'src': spotify_uri['Image'],
+                'placement': 'appLogoOverride'
+            }
 
-        #Prints the song added to the queue and sends a notification
-        print(f"\nAdded {spotify_uri['Title']} by {spotify_uri['Artist']} to queue.")
-        #notify('xmReader: Up Next!', f"{spotify_uri['Title']} by {spotify_uri['Artist']}", icon=spotify_uri['Image'])
+            print(f"Added {spotify_uri['Title']} by {spotify_uri['Artist']} to queue.")
+            #notify('xmSync: Up Next!', f"{spotify_uri['Title']} by {spotify_uri['Artist']}", icon=spotify_uri['Image'])
+        except spotipy.exceptions.SpotifyException as e:
+            if e.http_status == 404 and 'No active device' in str(e):
+                print("No active device found. Please start playing music on a device and try again.")
+                changeStation()
+            else:
+                print(f"An error occurred: {e}")
         
     return spotify_uri
 
 def main():
     global global_buffer, global_stationID
+    
+    #Initializes Spotify OAuth first, if it fails, the program will exit.
+    oauthConnection()
 
-    welcomeMessage = r"""
- ___  ___ ___      ___  _______   _______      __      ________   _______  _______   
-|"  \/"  |"  \    /"  |/"      \ /"     "|    /""\    |"      "\ /"     "|/"      \  
- \   \  / \   \  //   |:        (: ______)   /    \   (.  ___  :|: ______):        | 
-  \\  \/  /\\  \/.    |_____/   )\/    |    /' /\  \  |: \   ) ||\/    | |_____/   ) 
-  /\.  \ |: \.        |//      / // ___)_  //  __'  \ (| (___\ ||// ___)_ //      /  
- /  \   \|.  \    /:  |:  __   \(:      "|/   /  \\  \|:       :|:      "|:  __   \  
-|___/\___|___|\__/|___|__|  \___)\_______|___/    \___|________/ \_______)__|  \___) """
+    timeout = os.getenv('TIMEOUT')
+    start_time = datetime.now()
+    
+    welcomeMessage = r""""                                                                                               
+___   ___ .___  ___.      _______.____    ____ .__   __.   ______ 
+\  \ /  / |   \/   |     /       |\   \  /   / |  \ |  |  /      |
+ \  V  /  |  \  /  |    |   (----` \   \/   /  |   \|  | |  ,----'
+  >   <   |  |\/|  |     \   \      \_    _/   |  . `  | |  |     
+ /  .  \  |  |  |  | .----)   |       |  |     |  |\   | |  `----.
+/__/ \__\ |__|  |__| |_______/        |__|     |__| \__|  \______|
+                                                                """
     
     print(welcomeMessage)
     
-    print("\nThank you for using xmReader!\nTo Exit or Stop, Press 'q'\nTo Change Station, Press 'c'")
+    print("\nThank you for using xmSync!\nTo Exit or Stop, Press 'q'\nTo Change Station, Press 'c'")
     changeStation()
     
     while True:
@@ -93,17 +98,46 @@ def main():
                 changeStation()
         getSongLink()
 
+        #Check out timer
+        if timeout:
+            #Compare our current time against our start time
+            current_time = datetime.now()
+
+            #Calculate difference in seconds
+            time_difference = (current_time - start_time).total_seconds()
+
+            if time_difference > ord(timeout):
+                print("xmSync Closed due to Timeout Configuration")
+                sys.exit()
+
 def changeStation():
     global global_stationID
+
     print("Please Enter The Station Name Below!")
     while True:
         station = input()
 
         if station in stations:
             global_stationID = stations[station]
+            print("\n")
             break
         else:
             print("Invalid Station, Try Again or Refer to stations.py")
     
+def oauthConnection():
+    global global_sp
 
+    try:
+        load_dotenv()
+        auth_manager = SpotifyOAuth(
+        client_id=os.getenv('SPOTIFY_CLIENT_ID'),
+        client_secret=os.getenv('SPOTIFY_CLIENT_SECRET'),
+        redirect_uri=os.getenv('REDIRECT_URI'),
+        scope='user-modify-playback-state'
+    )
+    except Exception as e:
+        print("Error initializing Spotify OAuth, Check That .env is Properly Completed" )
+        sys.exit(1)
+
+    global_sp = spotipy.Spotify(auth_manager=auth_manager)
 main()
